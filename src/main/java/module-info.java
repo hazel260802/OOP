43ab0dc854b54a62caf6ea0c04db1e1233a39c9f
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-01-07 12:49:29 +0700

    add jackson to project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,13 @@
 module oop_lecture {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
 
 
-    opens oop_lecture to javafx.fxml;
+    // opens oop_lecture to javafx.fxml;
     exports oop_lecture.application;
-    opens oop_lecture.application to javafx.fxml;
+    opens oop_lecture.application;
+
     exports oop_lecture.controllers;
-    opens oop_lecture.controllers to javafx.fxml;
+    opens oop_lecture.controllers;
 }
