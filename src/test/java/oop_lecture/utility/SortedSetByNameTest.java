a039f7298c561cb1848bb33484ed3a9d09d2579b
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-01-10 13:20:06 +0700

    imply set.find

---
+++ src/test/java/oop_lecture/utility/SortedSetByNameTest.java
@@ -0,0 +1,36 @@
+package oop_lecture.utility;
+
+import oop_lecture.models.NhanVatLichSu;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortedSetByNameTest {
+
+	@Test
+	void find() {
+		SortedSetByName<NhanVatLichSu> sortedSetByName = new SortedSetByName<>();
+		var bht = new NhanVatLichSu("BHT", null, null, null, null, null);
+		var lht = new NhanVatLichSu("LHT", null, null, null, null, null);
+		var nna = new NhanVatLichSu("NNA", null, null, null, null, null);
+		var hbd = new NhanVatLichSu("HBD", null, null, null, null, null);
+		// empty set
+		assertNull(sortedSetByName.find("BHT"));
+
+		// set 1
+		sortedSetByName.add(bht);
+		assertEquals(bht, sortedSetByName.find("BHT"));
+
+		// set many
+		sortedSetByName.add(lht);
+		sortedSetByName.add(hbd);
+		sortedSetByName.add(nna);
+		assertEquals(bht,sortedSetByName.find("BHT"));
+		assertEquals(lht,sortedSetByName.find("LHT"));
+		assertEquals(hbd,sortedSetByName.find("HBD"));
+		assertEquals(nna,sortedSetByName.find("NNA"));
+
+		// not in set
+		assertNull(sortedSetByName.find("MNNH"));
+	}
+}
