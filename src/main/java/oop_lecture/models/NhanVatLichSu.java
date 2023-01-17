d61e8c9a8c2cbaad1c40e73abc85fc5d46501fdd
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-01-17 23:08:18 +0700

    hậu kiểm

--- src/main/java/oop_lecture/models/NhanVatLichSu.java
+++ src/main/java/oop_lecture/models/NhanVatLichSu.java
@@ -1,11 +1,8 @@
 package oop_lecture.models;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-import selenium.NhanVatLichSu;
 
 public class NhanVatLichSu extends LichSuCoTen {
 	  private List<String> suKienLichSu = new ArrayList<>();
