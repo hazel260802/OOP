f2f13a4971944035ed8e6685d339d8581f5243cf
Hữu Phúc <92777379+huuphuc02@users.noreply.github.com>
2023-01-17 10:55:36 +0700

    Update SuKienLichSu.java

--- src/main/java/oop_lecture/models/SuKienLichSu.java
+++ src/main/java/oop_lecture/models/SuKienLichSu.java
@@ -2,51 +2,78 @@ package oop_lecture.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.time.LocalDate;
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SuKienLichSu extends LichSuCoTen {
-    private LocalDate time;
-    private String description;
-    private Place place;
+    private Year namBatDau, namKetThuc;
+    private String moTa;
 
     private TrieuDai trieuDai;
     private String tenTrieuDai;
+    
+    private List<DiaDiem> diaDiemLienQuan = new ArrayList<>();
+    private List<String> tenDiaDiemLienQuan = new ArrayList<>();
 
     private List<NhanVatLichSu> nhanVatLienQuan = new ArrayList<>();
     private List<String> tenNhanVatLienQuan = new ArrayList<>();
 
 
-    public SuKienLichSu(String ten, LocalDate time, String description, Place place, String tenTrieuDai, List<String> tenNhanVatLienQuan) {
-        super(ten);
-        this.time = time;
-        this.description = description;
-        this.place = place;
-        this.tenTrieuDai = tenTrieuDai;
-        this.tenNhanVatLienQuan = tenNhanVatLienQuan;
-    }
-
-    public LocalDate getTime() {
-        return time;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Place getPlace() {
-        return place;
-    }
-
-    public TrieuDai getTrieuDai() {
-        return trieuDai;
-    }
-
-    public List<NhanVatLichSu> getNhanVatLienQuan() {
-        return nhanVatLienQuan;
-    }
+    
+    public SuKienLichSu(String ten) {
+		super(ten);
+	}
 
 
-    // TODO: link
+
+	public SuKienLichSu(String ten, Year namBatDau, Year namKetThuc, String moTa, TrieuDai trieuDai,
+			List<DiaDiem> diaDiemLienQuan, List<NhanVatLichSu> nhanVatLienQuan) {
+		super(ten);
+		this.namBatDau = namBatDau;
+		this.namKetThuc = namKetThuc;
+		this.moTa = moTa;
+		this.trieuDai = trieuDai;
+		this.diaDiemLienQuan = diaDiemLienQuan;
+		this.nhanVatLienQuan = nhanVatLienQuan;
+	}
+
+
+
+	public Year getNamBatDau() {
+		return namBatDau;
+	}
+
+	public Year getNamKetThuc() {
+		return namKetThuc;
+	}
+
+	public String getMoTa() {
+		return moTa;
+	}
+
+	public TrieuDai getTrieuDai() {
+		return trieuDai;
+	}
+
+	public String getTenTrieuDai() {
+		return tenTrieuDai;
+	}
+
+	public List<DiaDiem> getDiaDiemLienQuan() {
+		return diaDiemLienQuan;
+	}
+
+	public List<String> getTenDiaDiemLienQuan() {
+		return tenDiaDiemLienQuan;
+	}
+
+	public List<NhanVatLichSu> getNhanVatLienQuan() {
+		return nhanVatLienQuan;
+	}
+
+
+	public List<String> getTenNhanVatLienQuan() {
+		return tenNhanVatLienQuan;
+	}
 }
