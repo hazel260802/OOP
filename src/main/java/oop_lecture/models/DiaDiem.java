34ebcc8c981cf75a8a53547d0ee7716d3b21ae6d
Hữu Phúc <92777379+huuphuc02@users.noreply.github.com>
2023-01-17 10:51:02 +0700

    Update and rename Place.java to DiaDiem.java

---
+++ src/main/java/oop_lecture/models/DiaDiem.java
@@ -0,0 +1,16 @@
+package oop_lecture.models;
+
+public class DiaDiem {
+    public String chiTietDiaDiem;
+
+    public String ten;
+
+	public String getChiTietDiaDiem() {
+		return chiTietDiaDiem;
+	}
+
+	public String getTen() {
+		return ten;
+	}
+	
+}
