4a6e8548236ab80c521ddd4ba03f832823da6bad
TheBlueStar <bhtu.work@gmail.com>
2023-01-26 23:00:57 +0700

    fix for merging (#18) (#19)
    * Revert "sua tieng viet nhan vat lich su"
    
    * sua ten tv va xoa setter (#10)
    
    Co-authored-by: TheBlueStar <bhtu.work@gmail.com>
    
    * fix instruction
    
    * NNA rename attributes (#13)
    
    * Phuc doiten (#14)
    
    * Update DiaDiemLichSu.java
    
    * Update and rename Place.java to DiaDiem.java
    
    * Update LeHoiVanHoa.java
    
    * Update SuKienLichSu.java
    
    * Update DiaDiem.java
    
    remove getter update 
    add toString
    
    * Update DiaDiemLichSu.java
    
    remove get name 
    fix unachievable constructor
    
    * Update LeHoiVanHoa.java
    
    fix unaclievable constructor
    
    * Update SuKienLichSu.java
    
    Co-authored-by: TheBlueStar <bhtu.work@gmail.com>
    
    * hậu kiểm
    
    * controllers (#17)
    
    * Controllers
    
    * invoke home screen
    
    * define util buttons' actions
    
    * fix can't next after back
    
    * 30% DDLSC
    
    * finish DDLS
    
    * add place holder for DDLS table
    
    * ignore vscode folder
    
    * fix ST overflow
    
    * minor improve
    
    * add base and constructor to controllers
    
    * add row double click
    
    * move callback outside
    
    * fix after rename
    
    * NVLSController
    
    * rename package
    
    * move css link to code
    
    * fix declare icon for button
    
    * Home back-end
    
    * Update search results page
    
    * fix controller-view compatibility
    
    * abstractify lsct and utility.find_controller
    
    * fix no prev or next scene
    
    * fix DiaDiem null
    
    * reject empty search box
    
    * fix forward, backward btn
    
    * fix btn img
    
    Co-authored-by: Hazel260802 <lehuyentrang260802@gmail.com>
    
    * fix
    
    Co-authored-by: TheBlueStar <bhtu.work@gmail.com>
    Co-authored-by: NguyenNgocAnh20205228 <92777484+NguyenNgocAnh20205228@users.noreply.github.com>
    Co-authored-by: Hữu Phúc <92777379+huuphuc02@users.noreply.github.com>
    Co-authored-by: Hazel260802 <lehuyentrang260802@gmail.com>
    Co-authored-by: dinhhb <habuudinh@gmail.com>
    
    Co-authored-by: Hà Bửu Định <121761189+dinhhb@users.noreply.github.com>
    Co-authored-by: NguyenNgocAnh20205228 <92777484+NguyenNgocAnh20205228@users.noreply.github.com>
    Co-authored-by: Hữu Phúc <92777379+huuphuc02@users.noreply.github.com>
    Co-authored-by: Hazel260802 <lehuyentrang260802@gmail.com>
    Co-authored-by: dinhhb <habuudinh@gmail.com>

--- src/main/java/oop_lecture/models/LeHoiVanHoa.java
+++ src/main/java/oop_lecture/models/LeHoiVanHoa.java
@@ -1,7 +1,7 @@
 package oop_lecture.models;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class LeHoiVanHoa extends LichSuCoTen {
