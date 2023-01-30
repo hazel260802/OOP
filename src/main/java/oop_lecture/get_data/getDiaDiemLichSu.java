daf0b778bd3bbe0d112df4b67fb889673e423812
unknown <henrynnguyen02@gmail.com>
2023-01-30 19:52:24 +0700

    Cập nhật getDiaDiem và getDiaDiemLichSu

--- src/main/java/oop_lecture/get_data/getDiaDiemLichSu.java
+++ src/main/java/oop_lecture/get_data/getDiaDiemLichSu.java
@@ -1,6 +1,7 @@
 package oop_lecture.get_data;
 
 import oop_lecture.models.DiaDiemLichSu;
+import oop_lecture.models.DiaDiem;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -16,90 +17,50 @@ import java.util.Date;
 import java.util.List;
 
 public class getDiaDiemLichSu {
-	
-	public static void getHrefNThuVienLS(WebDriver driver,String web,List<String> listHref) {
+	public static void getThongTinDiaDiemLichSuWiki(WebDriver driver,String web,List<DiaDiemLichSu> listDiaDiemLichSu) {
 		driver.navigate().to(web);
-		int i = 0;
-		while(true) {
-			try {
-				List<WebElement> list=driver.findElements(By.xpath("/html/body/div[1]/div[3]/div[2]/div[1]/div/div/div/div/div/a"));
-		        list.forEach(e -> {
-		        	String href = e.getAttribute("href");
-		        	listHref.add(href);
-		        });
-		        
-		    	i=i+1;
-		    	//  Số thứ tự của trang cần lấy dữ liệu tiếp theo
-		    	System.out.println(i);
-		 	  	if( i == 2 ) break;
 		
-		 	  	WebElement ele = driver.findElement(By.xpath("//a[normalize-space()='»']"));
-		 	  	JavascriptExecutor executor = (JavascriptExecutor)driver;
-		 	  	executor.executeScript("arguments[0].click();", ele);
-		 	  	
-	    	} catch (Exception e) {
-	    		break;
+		for(int i = 2; i <= 62; i++) {
+			String diaDiem = driver.findElement(By.xpath("//h3[" + String.valueOf(i) + "]/span[@class='mw-headline']")).getText(); 
+			
+			List<String> listDiTich = new ArrayList<String>();
+			List<WebElement> diTichs = driver.findElements(By.xpath("//table[" + String.valueOf(i) + "]/tbody/tr//td[1]"));
+			for (WebElement diTich : diTichs) {
+				listDiTich.add(diTich.getText());
+			}
+			
+			List<DiaDiem> listViTri = new ArrayList<DiaDiem>();
+			List<WebElement> viTris = driver.findElements(By.xpath("//table[" + String.valueOf(i) + "]/tbody/tr//td[2]"));
+			for (WebElement viTri : viTris) {
+				DiaDiem p1 = new DiaDiem();
+				p1.ten = viTri.getText() + ", " + diaDiem;
+				listViTri.add(p1);
+			}
+			
+			List<String> listLoaiDiTich = new ArrayList<String>();
+			List<WebElement> loaiDiTichs = driver.findElements(By.xpath("//table[" + String.valueOf(i) + "]/tbody/tr//td[3]"));
+			for (WebElement loaiDiTich : loaiDiTichs) {
+				listLoaiDiTich.add(loaiDiTich.getText());
+			}
+			
+			for (int j = 0; j < listDiTich.size(); j++) {
+				DiaDiemLichSu dd = new DiaDiemLichSu(listDiTich.get(j));
+				dd.setDiaDiem(listViTri.get(j));
+				dd.setLoaiDiTich(listLoaiDiTich.get(j));
+				listDiaDiemLichSu.add(dd);
+				
+				System.out.println("Tên Di Tích : " + dd.getTen());
+				System.out.println("Địa điểm : " + dd.getDiaDiem().toString());
+				System.out.println("Loại Di Tích : " + dd.getLoaiDiTich() + "\n");
 			}
 		}
+		
 	}
-	
-	public static void GetThongTinDiaDiemThuVienLS(WebDriver driver,String href, List<DiaDiemLichSu> listDiaDiem) throws ParseException {
-		
-		List<String> listNhanVatLienQuanTamThoi = new ArrayList<String>();
-	    List<String> listSuKienLienQuanTamThoi = new ArrayList<String>();
-	    
-		driver.navigate().to(href);
-		
-		// Tên địa điểm
-		WebElement elementTenDiaDiem=driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[1]/h3[1]"));
-	    String tenDiaDiem = elementTenDiaDiem.getText().trim();
-	       
-	    // Diễn biến lịch sử
-	    WebElement elementDienBienLichSu=driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[2]/div[1]/div[3]/div[1]/div[2]"));
-	    String dienBienLichSu = elementDienBienLichSu.getText().trim();
-	            	  
-	    // Sự kiện liên quan
-	    try {
-		    WebElement suKienLienQuan = driver.findElement(By.xpath("//h3[contains(text(),'Sự kiện liên quan')]"));
-		    
-		    List<WebElement> tenSuKienLienQuan = driver.findElements(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[2]/div[1]/div[5]/div/div[1]/div[2]/div[1]/h4[1]"));  	
-		    
-		    tenSuKienLienQuan.forEach(e -> {
-		    	String Ten2 = e.getText().trim();
-		    	String[] Ten1 = Ten2.split("\\(");
-		    	listSuKienLienQuanTamThoi.add(Ten1[0].trim());
-		    });
-	    } catch (Exception e) {
-	    	
-	    }
-	    
-	    // Nhân vật liên quan
-	    try {
-		    WebElement nhanVatLienQuan = driver.findElement(By.xpath("//h3[contains(text(),'Nhân vật liên quan')]"));
-		    
-		    List<WebElement> tenNhanVatLienQuan = driver.findElements(By.xpath("/html[1]/body[1]/div[1]/div[3]/div[2]/div[1]/div[5]/div/div[1]/div[2]/div[1]/h4[1]"));  	
-		    
-		    tenNhanVatLienQuan.forEach(e -> {
-		    	String Ten2 = e.getText().trim();
-		    	String[] Ten1 = Ten2.split("\\(");
-		    	listNhanVatLienQuanTamThoi.add(Ten1[0].trim());
-		    });
-	    } catch (Exception e) {
-	    	
-	    }
-			
-		DiaDiemLichSu NhanVatTamThoi= new DiaDiemLichSu(); // TODO : làm lại models Địa điểm
-		
-		// thêm nhân vật vô list
-		listDiaDiem.add(NhanVatTamThoi); // TODO : adđ theo địa điểm trên <not complete>
-	}
-	
-	
 	
 	public static void main(String[] args) throws InterruptedException, ParseException {
 			System.setProperty("testHref.java", "UTF-8");
 			List<String> listHref= new ArrayList<String>();
-			List<DiaDiemLichSu> listDiaDiem = new ArrayList<DiaDiemLichSu>();
+			List<DiaDiemLichSu> listDiaDiemLichSu = new ArrayList<DiaDiemLichSu>();
 			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 //		    WebDriver driver1 = new ChromeDriver();
 		    ChromeOptions options = new ChromeOptions();
@@ -107,12 +68,10 @@ public class getDiaDiemLichSu {
 		    WebDriver driver1 = new ChromeDriver(options);
 		    
 		   
-		    String url ="https://thuvienlichsu.com/dia-diem";
+		    String url ="https://vi.wikipedia.org/wiki/Danh_s%C3%A1ch_Di_t%C3%ADch_qu%E1%BB%91c_gia_Vi%E1%BB%87t_Nam";
 		    
-		    getHrefNThuVienLS(driver1,url,listHref);
-		    for(int i=0;i<listHref.size();i++) {    	
-		    	GetThongTinDiaDiemThuVienLS(driver1,listHref.get(i),listDiaDiem);
-		    }
+		    getThongTinDiaDiemLichSuWiki(driver1, url, listDiaDiemLichSu);
+		    
 	        // Thoát
 	        driver1.quit();
 	        
@@ -120,4 +79,3 @@ public class getDiaDiemLichSu {
 	       
 	}
 }
-
