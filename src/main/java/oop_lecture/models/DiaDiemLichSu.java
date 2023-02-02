e37f2d5325a633bbe4287a0e7bbe2c719cf0c30e
Henry-n-Nguyen <70119608+Henry-n-Nguyen@users.noreply.github.com>
2023-02-02 07:58:06 +0700

    tmp (#32)
    Co-authored-by: unknown <henrynnguyen02@gmail.com>

--- src/main/java/oop_lecture/models/DiaDiemLichSu.java
+++ src/main/java/oop_lecture/models/DiaDiemLichSu.java
@@ -1,59 +1,35 @@
 package oop_lecture.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import oop_lecture.models.LichSuCoTen;
+import oop_lecture.models.DiaDiem;
 import oop_lecture.utility.SortedSetByName;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DiaDiemLichSu extends LichSuCoTen {
-    private DiaDiem diaDiem;
+	private DiaDiem diaDiem;
     private String loaiDiTich;
-    private String capDo;
-    private boolean laDiSanTheGioi;
-
-    private List<SuKienLichSu> suKienLichSu = new ArrayList<>();
-    private List<String> tenSuKienLichSu;
-
-
-    public DiaDiemLichSu(String ten, DiaDiem diaDiem, String loaiDiTich, String capDo, boolean laDiSanTheGioi, List<String> tenSuKienLichSu) {
-		super(ten);
-		this.diaDiem = diaDiem;
-		this.loaiDiTich = loaiDiTich;
-		this.capDo = capDo;
-		this.laDiSanTheGioi = laDiSanTheGioi;
-		this.tenSuKienLichSu = tenSuKienLichSu;
+    
+    public DiaDiemLichSu(String ten) {
+    	super(ten);
 	}
-
+       
+    public void setDiaDiem(DiaDiem diaDiem) {
+    	this.diaDiem = diaDiem;
+	}
+    
+    public void setLoaiDiTich(String loaiDiTich) {
+    	this.loaiDiTich = loaiDiTich;
+    }
+    
 	public DiaDiem getDiaDiem() {
 		return diaDiem;
 	}
 
 	public String getLoaiDiTich() {
 		return loaiDiTich;
-	}
-
-	public String getCapDo() {
-		return capDo;
-	}
-
-	public boolean getLaDiSanTheGioi() {
-		return laDiSanTheGioi;
-	}
-
-	public List<SuKienLichSu> getSuKienLichSu() {
-		return suKienLichSu;
-	}
-
-
-	/**
-	 * Tìm và liên kết với các object ở mảng khác
-	 */
-	public void link(SortedSetByName<SuKienLichSu> allSKLS) {
-		for (var tenSK : tenSuKienLichSu) {
-			var x = allSKLS.find(tenSK);
-			if (x != null) suKienLichSu.add(x);
-			else suKienLichSu.add(new SuKienLichSu(tenSK,null,null,null,null,null,null));
-		}
 	}
 }
--- src/main/java/oop_lecture/models/TrieuDai.java
+++ src/main/java/oop_lecture/models/TrieuDai.java
@@ -1,53 +1,51 @@
 package oop_lecture.models;
 
-import java.time.LocalDate;
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TrieuDai extends LichSuCoTen {
-    private String quocHieu;
-    private LocalDate batDau, ketThuc;
-    private DiaDiem thuDo;
+    private Year batDau, ketThuc;
+    private String kinhDo;
 
     private List<NhanVatLichSu> hoangDe = new ArrayList<>();
     private List<String> tenHoangDe = new ArrayList<>();
 
-    private List<SuKienLichSu> suKien = new ArrayList<>();
-    private List<String> tenSuKien = new ArrayList<>();
-
-
-    public TrieuDai(String ten, String quocHieu, LocalDate batDau, LocalDate ketThuc, DiaDiem thuDo, List<String> tenHoangDe, List<String> tenSuKien) {
-        super(ten);
-        this.quocHieu = quocHieu;
-        this.batDau = batDau;
-        this.ketThuc = ketThuc;
-        this.thuDo = thuDo;
-        this.tenHoangDe = tenHoangDe;
-        this.tenSuKien = tenSuKien;
+    public TrieuDai(String name) {
+        super(name);
+    }
+    
+    public void setTenHoangDe(List<String> tenHoangDe) {
+    	this.tenHoangDe = tenHoangDe;
+    }
+    
+    public void setThoiGian(Year namBatDau,Year namKetThuc ) {
+    	this.batDau = namBatDau;
+    	this.ketThuc = namKetThuc;
+    }
+    
+    public void setKinhDo(String kinhDo) {
+    	this.kinhDo = kinhDo;
     }
 
-    public String getQuocHieu() {
-        return quocHieu;
-    }
-
-    public LocalDate getBatDau() {
+    public Year getBatDau() {
         return batDau;
     }
 
-    public LocalDate getKetThuc() {
+    public Year getKetThuc() {
         return ketThuc;
     }
 
-    public DiaDiem getThuDo() {
-        return thuDo;
+    public String getKinhDo() {
+        return kinhDo;
     }
 
     public List<NhanVatLichSu> getHoangDe() {
         return hoangDe;
     }
-
-    public List<SuKienLichSu> getSuKien() {
-        return suKien;
+    
+    public List<String> getTenHoangDe() {
+        return tenHoangDe;
     }
 
     // TODO: 10/01/2023 link
